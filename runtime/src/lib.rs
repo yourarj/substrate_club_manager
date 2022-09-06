ef4a892c30d5ec357276ad4fdbfdb6e1badb12c6
yourarj <11359226+yourarj@users.noreply.github.com>
2022-09-07 00:35:36 +0530

    INITIAL COMMIT
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     On branch main
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	renamed:    pallets/template/Cargo.toml -> pallets/club-manager/Cargo.toml
    	renamed:    pallets/template/README.md -> pallets/club-manager/README.md
    	renamed:    pallets/template/src/benchmarking.rs -> pallets/club-manager/src/benchmarking.rs
    	renamed:    pallets/template/src/lib.rs -> pallets/club-manager/src/lib.rs
    	renamed:    pallets/template/src/mock.rs -> pallets/club-manager/src/mock.rs
    	renamed:    pallets/template/src/tests.rs -> pallets/club-manager/src/tests.rs
    	modified:   runtime/Cargo.toml
    	modified:   runtime/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -43,8 +43,8 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+/// Import the club manager pallet.
+pub use pallet_club_manager;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -265,8 +265,8 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+/// Configure the pallet-club-manager in pallets/club-manager.
+impl pallet_club_manager::Config for Runtime {
 	type Event = Event;
 }
 
@@ -286,8 +286,8 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		// Include the custom logic from the pallet-club-manager in the runtime.
+		ClubManagerModule: pallet_club_manager,
 	}
 );
 
@@ -332,7 +332,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_club_manager, ClubManagerModule]
 	);
 }
 
