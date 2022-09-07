ea7d7829e6a8c9a055344fe20c63767e32d1b8bb
yourarj <11359226+yourarj@users.noreply.github.com>
2022-09-07 18:39:36 +0530

    added benchmarks
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     On branch main
    
     Changes to be committed:
    	modified:   pallets/club-manager/src/benchmarking.rs

--- pallets/club-manager/src/benchmarking.rs
+++ pallets/club-manager/src/benchmarking.rs
@@ -3,18 +3,36 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as ClubManager;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use crate::Pallet as ClubManagerModule;
+use frame_benchmarking::{account, benchmarks};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Sudo(caller), s)
+	member_on_board {
+		let a in 0 .. 100_u32;
+		let b in 0 .. 2_u32;
+		let acc: T::AccountId = account("test", 0, a);
+		let club_id = T::ClubId::from(b);
+		// add club
+		Clubs::<T>::insert(club_id, ());
+	}: _(RawOrigin::Root, club_id, account("test", 0, a))
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert!(Membership::<T>::get(club_id, acc).is_some());
 	}
 
-	impl_benchmark_test_suite!(ClubManager, crate::mock::new_test_ext(), crate::mock::Test);
+	member_off_board {
+		let a in 0 .. 100_u32;
+		let b in 0 .. 2_u32;
+		let acc: T::AccountId = account("test", 0, a);
+		let club_id = T::ClubId::from(b);
+		// add respective club
+		Clubs::<T>::insert(club_id, ());
+		// on board member
+		ClubManagerModule::<T>::member_on_board(RawOrigin::Root.into(), club_id, acc.clone()).unwrap();
+	}: _(RawOrigin::Root, club_id, acc.clone())
+	verify {
+		assert!(Membership::<T>::get(club_id, acc).is_none());
+	}
+
+	impl_benchmark_test_suite!(ClubManagerModule, crate::mock::new_test_ext(), crate::mock::Test);
 }
